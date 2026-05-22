82b2ecf860a2c4869ab390bb0bd75f2bd9731fde
gnguyentran44 <gnguyentran44@gmail.com>
2026-05-22 13:45:37 +0700

    nộp bài lần 2

--- LAB2/lab2.sql
+++ LAB2/lab2.sql
@@ -1,10 +1,6 @@
-select first_name, last_name, state from sales.customers 
-where state = 'NY' order by first_name
+SELECT customer_id, first_name, last_name
+FROM sales.customers
+WHERE first_name LIKE 'W%' OR first_name LIKE '%a';
 
-select city, count(*) from sales.customers 
-where state = 'NY' group by city having count(*) >=10 order by city desc
 
-SELECT YEAR(order_date) AS order_year, COUNT(order_id) AS total_orders
-FROM Orders
-GROUP BY YEAR(order_date)
-ORDER BY order_year;
+
